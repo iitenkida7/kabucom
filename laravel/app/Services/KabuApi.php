7116edfc78211e9d15fc45afb768a4d26f09a394
iitenkida7 <i.takeshi2222@gmail.com>
2020-09-10 14:14:37 +0900

    発注系処理

--- laravel/app/Services/KabuApi.php
+++ laravel/app/Services/KabuApi.php
@@ -2,10 +2,13 @@
 
 namespace App\Services;
 
+
+
 class KabuApi
 {
     public $client;
     public $token = null;
+    public $orderInfo = null;
 
     public function __construct()
     {
@@ -58,5 +61,40 @@ class KabuApi
             return true;
         }
         return false;
+    }
+
+    public function sendOrder(): bool
+    {
+        $res = $this->client->request('POST', config('kabusapi.url') . '/sendorder', [
+            'headers' => ['X-API-KEY' => $this->token],
+            'json' => $this->orderInfo],
+            );
+          
+        if ($res->getStatusCode() === 200) {
+            return true;
+        }
+        return false;
+    }
+
+    public function orderInfoBuilder(int $exchange, int $side, string $symbol, int $qty, int $price) : object
+    {
+        $this->orderInfo = [
+            'Password' => config('kabusapi.orderPassword'),
+            'Symbol'   => $symbol,
+            'Exchange' => $exchange,
+            'SecurityType' => 1, //株式（固定）
+            'Side' => $side,
+            'CashMargin' => 1, // 1:現物, 2:信用新規, 3:信用返済
+             // 'MarginTradeType' => 0, // 現物省略化 1:制度信用, 2:一般信用, 3:一般信用（売短）
+            'DelivType' => 0,  //  0:指定なし, 1:自動振替, 2:お預り金
+            'FundType' => ' ', //  ' ':現物売、信用返済の場合  ※よくわからん。
+            'Qty' => $qty, // 数量
+            // 'ClosePositionOrder' => ,
+            // 'ClosePositions' => ,
+            'Price' => $price,
+            'ExpireDay' => 0, // 0:当日
+            'FrontOrderType' => 20 // 20:指値
+        ];
+        return $this;
     }
 }
--- laravel/config/kabusapi.php
+++ laravel/config/kabusapi.php
@@ -3,4 +3,5 @@
 return [
         'url' => env('KABU_API_URL'),
         'password' => env('KABU_API_PASSWORD'),
+        'orderPassword' => env('KABU_ORDER_PASSWORD'),
 ];
